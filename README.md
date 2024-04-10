86bc31829e1d7aa8819a4fafea7084debae6b7e4
shbashokian <shahramb7@gmail.com>
2024-04-10 15:55:40 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_ui2
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainApp(),
+    ));
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(),
+    );
+  }
+}
